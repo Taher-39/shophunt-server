8068b299f4e9f21ca58b3cbf45589b195ef11bd8
Abu <abut70243@gmail.com>
2022-02-01 21:53:48 +0600

    added user update api

--- controller/userController.js
+++ controller/userController.js
@@ -11,13 +11,16 @@ const authUser = asyncHandler(async (req, res) => {
   const user = await User.findOne({ email });
 
   if (user && (await user.matchPassword(password))) {
-    res.send({
+    res.json({
       _id: user._id,
       name: user.name,
       email: user.email,
       admin: user.isAdmin,
       token: generateToken(user._id),
     });
+  } else {
+    res.status(401);
+    throw new Error("Invalid password or email");
   }
 });
 
@@ -61,7 +64,7 @@ const registerUser = asyncHandler(async (req, res) => {
 const getUserProfile = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user._id);
   if (user) {
-    res.send({
+    res.json({
       _id: user._id,
       name: user.name,
       email: user.email,
@@ -73,4 +76,33 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, registerUser, getUserProfile };
+//desc   Update user profile
+//routes PUT api/users/profile
+//access private
+
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const updateUser = await user.save();
+
+    res.json({
+      _id: updateUser._id,
+      name: updateUser.name,
+      email: updateUser.email,
+      admin: updateUser.isAdmin,
+      token: generateToken(updateUser._id),
+    });
+
+  } else {
+    res.status(404);
+    throw new Error("User not Found");
+  }
+});
+
+export { authUser, registerUser, getUserProfile, updateUserProfile };
